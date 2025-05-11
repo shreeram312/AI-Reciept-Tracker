1d8a89282a924739c2e18bbcad7e1f855aa7d6ac
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2025-05-11 11:48:20 +0530

    feat: add landing page

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,15 @@
+"use client";
+
 import React from "react";
+import { Button } from "./ui/button";
 
 const Header = () => {
-  return <div className="text-3xl">Header</div>;
+  return (
+    <div className="text-3xl">
+      Header
+      <Button>Submit</Button>
+    </div>
+  );
 };
 
 export default Header;
